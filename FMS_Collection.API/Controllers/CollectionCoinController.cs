f721e1f02631c26433ba51e12ff2118a4aaa549c
FarukShaikh1 <farukshaikh908@gmail.com>
2025-09-28 16:44:50 +0530

    code correction

--- FMS_Collection.API/Controllers/CollectionCoinController.cs
+++ FMS_Collection.API/Controllers/CollectionCoinController.cs
@@ -32,7 +32,7 @@ public class CoinNoteCollectionController : ControllerBase
     }
 
 
-    [HttpPost]
+    [HttpGet]
     [Route("GetSummary")]
     public async Task<IActionResult> GetSummary()
     {
