36c9a9fbac559019d0acbfec590b7d587e25d122
Faruk Shaikh <faruk.shaikh@finbraine.com>
2026-08-25 18:11:05 +0530

    Removed web project

--- FMS_Collection.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FMS_Collection.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
